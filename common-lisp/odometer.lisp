c31799b61fb1242958ca4fa16dc8181cccaba039
mAdkins <software@doorways.org>
2023-05-19 15:35:35 -0700

    Reverse carry-over flag from two click methods

--- common-lisp/odometer.lisp
+++ common-lisp/odometer.lisp
@@ -6,12 +6,12 @@
      (chars :initarg :chars :reader chars)))
 
 ;; Click a wheel to its next character.
-;; Returns true if the wheel goes to a new character,
-;; or nil if the wheel repeats to the first character indicating carry-over.
+;; Returns true if the wheel returns to its first character,
+;; indicating carry-over to the next wheel.
 (defmethod click ((whl wheel))
   (if (>= (index whl) (1- (length (chars whl))))
-      (progn (setf (index whl) 0) nil)
-      (progn (setf (index whl) (1+ (index whl))) t)))
+      (progn (setf (index whl) 0) t)
+      (progn (setf (index whl) (1+ (index whl))) nil)))
 
 ;; Return the current character showing on the wheel.
 (defmethod current ((whl wheel))
@@ -45,17 +45,17 @@
 ;; Click odometer to its next setting.
 ;; Click wheels from end, continuing as long as
 ;; individual wheels signal carry-over by returning true.
-;; Returns nil if the first wheel returns nil
+;; Returns true if the last (leftmost) wheel clicked returns true,
 ;; indicating all wheels have carried over.
 (defmethod click ((mtr meter))
   (mapc #'(lambda (whl)
-            (when (click whl)
-                  (return-from click t)))
+            (unless (click whl)
+              (return-from click nil)))
     (reverse (wheels mtr)))
-  nil)
+  t)
 
 ;; Odometer solution to T9 problem.
 (defun odometer (item)
   (let ((mtr (make-meter item)))
     (loop collect (current mtr)
-          while (click mtr))))
+          until (click mtr))))
